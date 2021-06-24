6030a2055eb9b1809d83066404aad999abb72c42
Gemuele Aludino <g.aludino@gmail.com>
2021-06-23 23:27:58 -0400

    Created directory structure and sample code

---
+++ src/header.h
@@ -0,0 +1,14 @@
+/*!
+    \file       header.h
+    \brief      Header file
+
+    \author
+    \date
+ */
+
+#ifndef HEADER_H
+#define HEADER_H
+
+void say_hello(void);
+
+#endif /* HEADER_H */
---
+++ src/source.c
@@ -0,0 +1,18 @@
+/*!
+    \file       source.c
+    \brief      Source file
+
+    \author     
+    \date
+ */
+
+#include "header.h"
+
+#include <stdio.h>
+
+/*!
+    \brief      Prints "Hello, world!" to stdout
+ */
+void say_hello(void) {
+    puts("Hello, world!");
+}
